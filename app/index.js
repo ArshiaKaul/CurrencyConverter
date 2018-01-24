704be369a5b83319566982beac04982c892b08f1
Arshia Kaul <arshiakaul09@gmail.com>
2018-01-24 21:31:49 +0530

    CheckMark

--- app/index.js
+++ app/index.js
@@ -5,9 +5,10 @@ import CurrencyList from './screens/CurrencyList';
 EStyleSheet.build({
   $primaryBlue: '#FF6D7A',
   $white: '#FFFFFF',
-  $border: '#E2E2E2',
+  $border: '#929292',
   $inputText: '#797979',
   $lightGray: '#F3F3F3',
+  $darkText: '#343434',
 });
 
 export default () => <CurrencyList />;
