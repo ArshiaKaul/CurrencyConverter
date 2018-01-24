f390bc8192be335a418da2e215950e9cabda3a69
Arshia Kaul <arshiakaul09@gmail.com>
2018-01-24 19:32:50 +0530

    Add CurrencyList (FlatList)

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import Home from './screens/Home';
+import CurrencyList from './screens/CurrencyList';
 
 EStyleSheet.build({
   $primaryBlue: '#FF6D7A',
@@ -10,4 +10,4 @@ EStyleSheet.build({
   $lightGray: '#F3F3F3',
 });
 
-export default () => <Home />;
+export default () => <CurrencyList />;
